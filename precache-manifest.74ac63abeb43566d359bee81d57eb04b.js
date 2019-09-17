d3a969a94d3630db316f8f9f370688dd76785422
Incendie <incendie323@gmail.com>
2019-09-16 22:04:46 -0400

    Updates

---
+++ precache-manifest.74ac63abeb43566d359bee81d57eb04b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff6a6d3b4124e5c4c244e71a6b1d2cc",
+    "url": "/blue-jays-project/index.html"
+  },
+  {
+    "revision": "aa3d3bc224ed801caaef",
+    "url": "/blue-jays-project/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b7bd71f8057d4ef593b8",
+    "url": "/blue-jays-project/static/js/2.1436106e.chunk.js"
+  },
+  {
+    "revision": "aa3d3bc224ed801caaef",
+    "url": "/blue-jays-project/static/js/main.ad8e1190.chunk.js"
+  },
+  {
+    "revision": "14adbd3d5c847312fba0",
+    "url": "/blue-jays-project/static/js/runtime~main.215e449a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blue-jays-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
