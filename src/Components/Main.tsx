cae7d10ddcf0e1fb401b23e0af051d28c14d3d6f
Anson Li <ali@exchangesolutions.com>
2019-09-18 16:15:51 -0400

    small cleanup of files

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { Provider } from 'react-redux';
-import store from '../store';
 import Teams from './Teams/Teams';
 
 import { filterSubmitted } from '../Actions/filterActions';
